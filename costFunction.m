871e7634c8f281a9ad15337b3229182b6ec5dc7f
ymythebest <46920500+ymythebest@users.noreply.github.com>
2019-08-08 21:46:17 +0800

    Create costFunction.m

---
+++ costFunction.m
@@ -0,0 +1,11 @@
+function [J, grad] = costFunction(theta, X, y)
+
+J = 0;
+grad = zeros(size(theta));
+
+h = sigmoid(X*theta);
+% J = (1/m)*sum(-y .* log(h) - (1 - y) .* log(1-h));
+J = (1/m)*(-y'* log(h) - (1 - y)'* log(1-h));
+grad = (1/m)*X'*(h - y);
+
+end
