d88cd180c14987311219f022eb2629042db20773
ymythebest <46920500+ymythebest@users.noreply.github.com>
2019-08-08 21:47:11 +0800

    Create costFunctionReg.m

---
+++ costFunctionReg.m
@@ -0,0 +1,15 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+
+m = length(y);
+J = 0;
+grad = zeros(size(theta));
+
+h = sigmoid(X*theta);
+shift_theta = theta(2:size(theta));
+theta_reg = [0;shift_theta];
+
+J = (1/m)*(-y'* log(h) - (1 - y)'*log(1-h))+(lambda/(2*m))*theta_reg'*theta_reg;
+
+grad = (1/m)*(X'*(h-y)+lambda*theta_reg);
+
+end
